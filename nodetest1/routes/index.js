085c3ebb91dba9981f1f44d861e93fd271a20eef
masteropack <opackppp@gmail.com>
2019-02-05 19:02:27 +0700

    change homepage

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('newuser', { title: 'BlueEntry' });
 });
 
 router.get('/helloworld', function(req, res, next) {
