885dcb7944672f576a4cd1f7980fdd1d0f8df4a9
Thanakrit Buranakarn <Guy_Krups@hotmail.com>
2019-04-20 00:04:30 +0700

    updateOrg

---
+++ nodetest1/public/javascripts/editprofile.js
@@ -0,0 +1,26 @@
+function toInput() {
+    var ip = document.querySelectorAll('#myInput');
+    var tf = document.querySelectorAll('#tf');
+    document.getElementById('editbtn').style.display='none';
+    document.getElementById('savebtn').style.display='block';
+    for (var i = 0; i < tf.length; i++) {
+        if (ip[i].style.display === "none") {
+            ip[i].value = tf[i].innerHTML;
+            tf[i].style.display = "none";
+            ip[i].style.display = "block";
+        }
+    }
+}
+function toSubmit() {
+    var ip = document.querySelectorAll('#myInput');
+    var tf = document.querySelectorAll('#tf');
+    for (var i = 0; i < tf.length; i++) {
+        if (ip[i].style.display != "none") {
+            tf[i].innerHTML = ip[i].value;
+            tf[i].style.display = "block";
+            ip[i].style.display = "none";
+        }
+    }
+    document.getElementById('editbtn').style.display='block';
+    document.getElementById('savebtn').style.display='none';
+}
